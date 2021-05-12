d0779cb5b18e7ec165082898de600d8ed16dfb29
SHT-3756 <gkdlfndkekd@naver.com>
2021-05-12 20:45:55 +0900

    '헤더,뱃지추가'

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ searchInputEl.addEventListener('blur', function() {
     searchInputEl.setAttribute('placeholder', '');
     
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if(window.scrollY) {
+        //badge 숨기기
+        // gsap.to(요소, 지속시간, 옵션); 
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        }); 
+    }else{
+       //badge 보이기 
+       gsap.to(badgeEl, .6, {
+        opacity: 1,
+        display: 'block'
+    }); 
+    }
+}, 300));
+// -.throttle(함수, 시간)
