818daf4d27ca3c6b0c5f85b867211d302d01ee57
SHT-3756 <gkdlfndkekd@naver.com>
2021-05-14 18:49:03 +0900

    'awards다중슬라이드추가'

--- js/main.js
+++ js/main.js
@@ -68,9 +68,22 @@ new Swiper('.promotion .swiper-container', {
     },
     navigation: {
         prevEl: '.promotion .swiper-prev',
-        nextEl: '.promotion .swiper-next',
+        nextEl: '.promotion .swiper-next'
     }
 });
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
+
+
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
